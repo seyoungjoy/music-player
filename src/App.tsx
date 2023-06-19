b7135eb9c490e6c9c01a0bfa94c68c95dd20fd62
seyoungjoy <diddpwl80@gmail.com>
2023-06-19 10:05:50 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>플레이리스트 페이지를 작성해주세요.</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import createMockServer from './mockServer/createMockServer';
+
+// 수정 및 삭제 금지
+createMockServer();
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root'),
+);
