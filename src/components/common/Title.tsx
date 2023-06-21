96166ddd5aaa2e6a0e38ee2cd08f5ec7af12b9c0
seyoungjoy <diddpwl80@gmail.com>
2023-06-21 11:08:40 +0900

    feat: 타이틀 수정

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { S } from './Title.styled';
 const Title = () => {
-  return <S.H1>플레이리스트</S.H1>;
+  return <S.H1>Youngs Music</S.H1>;
 };
 
 export default React.memo(Title);
