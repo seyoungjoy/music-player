4126475fb1507bf282d316cbe4fadb5fd63a9f4e
seyoungjoy <diddpwl80@gmail.com>
2023-06-23 21:27:12 +0900

    remove: 미사용 파일 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,6 @@ export { default as Title } from './common/Title';
 export { default as PlayToggleButton } from './common/PlayToggleButton';
 export { default as Spin } from './common/Spin';
 
-export { default as Error } from './error/Error';
-export { default as ErrorBoundary } from './error/ErrorBoundary';
-
 export { default as MusicList } from './music/MusicList';
 export { default as MusicItem } from './music/MusicItem';
 export { default as MusicPlayer } from './music/MusicPlayer';
